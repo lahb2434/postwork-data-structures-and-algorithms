67cbcc13a783b2c0dd105eb8ecc5e47da68284d1
Christoper Rean Houghton <chrishoughton2434@gmail.com>
2022-04-06 14:08:03 -0400

    add function, psuedocode

---
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript1/reverse_string.js
@@ -0,0 +1,23 @@
+function reverseString(str) {
+  changethings in here
+}
+
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: 'ih'");
+  console.log("=>", reverseString("hi"));
+
+  console.log("");
+
+  console.log("Expecting: 'ybabtac'");
+  console.log("=>", reverseString("catbaby"));
+}
+
+module.exports = reverseString;
+
+
+// splitString = [s t r i n g]
+// create new empty array newArray = []
+// iterate over splitString and take index of element
+// add element to newArray at the negative index of splitString element
+
