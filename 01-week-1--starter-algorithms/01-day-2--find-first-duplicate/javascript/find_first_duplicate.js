00ac45eaf115acd84f0740a80827cb35d4a85617
Chris Houghton <chrishoughton2434@gmail.com>
2022-04-09 11:58:22 -0400

    done

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,12 +1,24 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  const newArr =  [...Array(arr.length).keys()]
+  const duplicate = arr.find((elem, index) => {
+    index !== newArr[index]
+    return elem
+  })
+  if(!duplicate){
+    return -1
+  }
+  return duplicate
 }
 
 if (require.main === module) {
+  let arr1 = ['a','b','c','d','e','c','f','g','a']
+  console.log("Expecting: c");
+  console.log("=>", findFirstDuplicate(arr1));
+
   // add your own tests in here
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
-
+  
   console.log("");
 
   console.log("Expecting: -1");
@@ -17,12 +29,9 @@ module.exports = findFirstDuplicate;
 
 //pseudocode:
 // function findFirstDuplicate
-//   duplicate Array
-//   iterate over first array 
-//   nest second iteration of second Array
-//   if first array element is equal to second array element
-//   stop iteration 
-//   return duplicated element
+//   create Array populated up to the length of arr
+//   compare the new array value with the index of arr
+//   if new array value isnt equal to arr index value return value
 // done
 
 
