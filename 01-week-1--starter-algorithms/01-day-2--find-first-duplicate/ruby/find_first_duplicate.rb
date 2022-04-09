d8bf62104146c73948675cc42e9f9894bc8a5803
Chris Houghton <chrishoughton2434@gmail.com>
2022-04-09 13:23:17 -0400

    done

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
@@ -1,5 +1,13 @@
 def find_first_duplicate(arr)
-  # type your code in here
+  arr2 = []
+  
+  duplicate = arr.each do |x|
+    if  arr2.include?(x)
+      return x;
+    end
+    arr2<<x
+  end
+ -1
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -12,6 +20,21 @@ if __FILE__ == $PROGRAM_NAME
   puts "=>", find_first_duplicate([1, 2, 3, 4])
 
   # Don't forget to add your own!
+
+  puts "Expecting: 7"
+  puts "=>", find_first_duplicate([1, 7, 2, 3, 4, 7, 4])
+  
+  puts
+
+  puts "Expecting: 9"
+  puts "=>", find_first_duplicate([1, 9, 2, 5, 7, 8, 9, 6])
+  
+  puts
+
+  puts "Expecting: -1"
+  puts "=>", find_first_duplicate([7, 4, 6, 9])
+  
+  puts
 end
 
 # Please add your pseudocode to this file
