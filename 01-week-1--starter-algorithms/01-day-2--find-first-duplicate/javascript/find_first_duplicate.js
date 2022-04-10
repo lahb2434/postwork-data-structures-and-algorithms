c488e631568747c266962cd8b1886c2179b67d34
Chris Houghton <chrishoughton2434@gmail.com>
2022-04-10 15:05:06 -0400

    done

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -31,8 +31,17 @@ module.exports = findFirstDuplicate;
 // function findFirstDuplicate
 //   create Array populated up to the length of arr
 //   compare the new array value with the index of arr
-//   if new array value isnt equal to arr index value return value
+//   return value if not equal
+//   if no value returned return -1
 // done
 
 
 // And a written explanation of your solution
+//
+// I create a new array with a range of 0..arr.length, so now I have the argument 
+// array and a new array, I then iterate through the arr with .find(),
+// I compare the index of each element to the value of the new array 
+// knowing the arr.indexOf() function will return the index of the first 
+// instance of the element in the array. If there a duplicate the index and the 
+// new array value will not be equal returning the element.
+//
